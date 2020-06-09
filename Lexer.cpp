60f9b47c83fd148c966692a31c68c6ae2899946f
sandwichdoge <sandwichdoge@gmail.com>
2020-06-10 00:43:36 +0700

    Support quotes for Lexer

--- Lexer.cpp
+++ Lexer.cpp
@@ -12,6 +12,9 @@ int Lexer::lex(const std::string& code, std::vector<std::string>& tokens) {
     if (tokenize(mutableCode, tokens) < 0) {
         return -2;
     }
+    for (std::size_t i = 0; i < tokens.size(); ++i) {
+        std::cout << tokens[i] << "\n";
+    }
     return 0;
 }
 
@@ -23,33 +26,56 @@ int Lexer::sanitize(std::string& code) {
 
 int Lexer::tokenize(const std::string& code, std::vector<std::string> &tokens) {
     // Tokenize into words
-    bool isWord = false;
+    bool isQuotes = false; // Whether we're in a pair of quotes.
+    bool isWord = false;   // Whether we're in a word.
     std::size_t wordLen = 0;
     std::size_t wordStart = 0;
     for (std::size_t i = 0; i < code.size(); ++i) {
-        if (isParenthesis(code[i])) { // Parenthesis met
-            if (isWord) {
-                std::string token = code.substr(wordStart, wordLen);
-                tokens.push_back(token);
-                isWord = false;
-            }
-            tokens.push_back(code.substr(i, 1));
-        }
-        else if (isDelimChar(code[i])) {  // Delim char met, i.e end of word
-            if (isWord) {
-                std::string token = code.substr(wordStart, wordLen);
-                tokens.push_back(token);
-                isWord = false;
-            }
-        } else { // Ascii char met
-            if (isWord) {  // Already in word
-                wordLen++;
-            } else {
-                wordStart = i;
+        if (code[i] == '"') { // Quotes met
+            if (!isWord) {    // Initial value
+                tokens.push_back("\"");
                 isWord = true;
-                wordLen = 1;
+            }
+            if (isQuotes) {
+                std::string token = code.substr(wordStart, wordLen);
+                tokens.push_back(token);
+                tokens.push_back("\"");
+                isWord = false;
+            } else {
+                wordStart = i + 1;
+            }
+            isQuotes = !isQuotes;
+            continue;
+        }
+
+        if (isQuotes) {
+            wordLen++;
+        } else {
+            if (isParenthesis(code[i])) { // Parenthesis met
+                if (isWord) {
+                    std::string token = code.substr(wordStart, wordLen);
+                    tokens.push_back(token);
+                    isWord = false;
+                }
+                tokens.push_back(code.substr(i, 1));
+            }
+            else if (isDelimChar(code[i])) {  // Delim char met, i.e end of word
+                if (isWord) {
+                    std::string token = code.substr(wordStart, wordLen);
+                    tokens.push_back(token);
+                    isWord = false;
+                }
+            } else { // Ascii char met
+                if (isWord) {  // Already in word
+                    wordLen++;
+                } else {
+                    wordStart = i;
+                    isWord = true;
+                    wordLen = 1;
+                }
             }
         }
+        
     }
     return 0;
 }
