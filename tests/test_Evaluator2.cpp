29da0d67be0ef2b0b9d09a815f4070e726e2f535
Thuan Ho <sandwichdoge@gmail.com>
2020-06-11 15:42:54 +0700

    add more eval tests

--- tests/test_Evaluator2.cpp
+++ tests/test_Evaluator2.cpp
@@ -6,3 +6,86 @@
 TEST(Evaluator, Sane) {
     EXPECT_EQ(-1, -1);
 }
+
+TEST(Evaluator, nothing) {
+    std::string code = "( () () )";
+    Lexer pLexer;
+    std::vector<std::string> tokens;
+    pLexer.lex(code, tokens);
+
+    Parser pParser;
+    pParser.setTokens(tokens);
+    SyntaxTreeNode *root = new SyntaxTreeNode;
+    pParser.parse(root);
+
+    Evaluator pEvaluator;
+    double ret = pEvaluator.eval(root);
+    EXPECT_FLOAT_EQ(ret, 0);
+
+    root->cleanSyntaxTree();
+    delete root;
+}
+
+TEST(Evaluator, nothing2) {
+    std::string code = "";
+    Lexer pLexer;
+    std::vector<std::string> tokens;
+    pLexer.lex(code, tokens);
+
+    Parser pParser;
+    pParser.setTokens(tokens);
+    SyntaxTreeNode *root = new SyntaxTreeNode;
+    pParser.parse(root);
+
+    Evaluator pEvaluator;
+    double ret = pEvaluator.eval(root);
+    EXPECT_FLOAT_EQ(ret, 0);
+
+    root->cleanSyntaxTree();
+    delete root;
+}
+
+TEST(Evaluator, fault) {
+    std::string code = "(< (1) (2) (3))";
+    Lexer pLexer;
+    std::vector<std::string> tokens;
+    pLexer.lex(code, tokens);
+
+    Parser pParser;
+    pParser.setTokens(tokens);
+    SyntaxTreeNode *root = new SyntaxTreeNode;
+    pParser.parse(root);
+
+    bool testSuccess = false;
+    Evaluator pEvaluator;
+    try {
+        double ret = pEvaluator.eval(root);
+    } catch (...) {
+        testSuccess = true;
+    }
+    EXPECT_EQ(testSuccess, true);
+
+    root->cleanSyntaxTree();
+    delete root;
+}
+
+TEST(Evaluator, logical_ops) {
+    std::string code = "( && \
+    (== 5 5) \
+    (< (1) 2))";
+    Lexer pLexer;
+    std::vector<std::string> tokens;
+    pLexer.lex(code, tokens);
+
+    Parser pParser;
+    pParser.setTokens(tokens);
+    SyntaxTreeNode *root = new SyntaxTreeNode;
+    pParser.parse(root);
+
+    Evaluator pEvaluator;
+    double ret = pEvaluator.eval(root);
+    EXPECT_FLOAT_EQ(ret, 1);
+
+    root->cleanSyntaxTree();
+    delete root;
+}
